611dcb75ed02fc6063b7074329c8a834a93e430b
AugustUno <johngemare@gmail.com>
2026-06-25 09:13:38 +0800

    hostinger troubleshooting

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-Check lng
 | Date | Modules Affected | Notes |
 | :--- | :---: | :--- |
 | June 4 | Admin sidebar, homepage, and room manage | <ul><li>Admin sidebar edits</li><li>Admin homepage partial edits (lacking)</li><li>Admin room manage full edits</li></ul> |
--- pages/faculty-signup.php
+++ pages/faculty-signup.php
@@ -18,6 +18,15 @@
         }
     }
 ?>
+
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+if (session_status() === PHP_SESSION_NONE) session_start();
+// ... rest of file
+?>
 <!DOCTYPE html>
 <html lang="en">
 
