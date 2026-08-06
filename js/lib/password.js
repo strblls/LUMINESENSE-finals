f066e8009a3349b44c4f505f3433c70da1658999
Alexandra <stereoballsgrande@gmail.com>
2026-08-06 23:01:02 +0800

    form

--- js/lib/password.js
+++ js/lib/password.js
@@ -1,6 +1,7 @@
 function setupToggle(toggleId, inputId) {
     const toggle = document.querySelector(`#${toggleId}`);
     const input = document.querySelector(`#${inputId}`);
+    if (!toggle || !input) return;
     toggle.addEventListener('click', (e) => {
         const type = input.getAttribute('type') === 'password' ? 'text' : 'password';
         input.setAttribute('type', type);
